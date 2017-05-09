09c5dd8e880c6023857c1df545d3c457511e814f
Steve Mills <stevepatrickmills@gmail.com>
2017-05-09 15:12:46 -0500

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,12 +29,13 @@ socket.on('newMessage', function(message) {
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+  var messageTextBox = jQuery('[name=message]')
 
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
-
+    messageTextBox.val('')
   });
 });
 
@@ -45,13 +46,17 @@ locationButton.on('click', function(){
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
     console.log(position);
   }, function () {
+      locationButton.removeAttr('disabled').text('Send location');
       alert('Unable to fetch location');
   });
 });
